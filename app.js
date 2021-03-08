230ad16f978c8c14ebd91b96b0634cd91718c31e
jmartinezcl <martinez.alvarez.jose77@gmail.com>
2021-03-08 08:55:09 -0300

    add app.js - express api

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const bookRouter = express.Router();
+const port = process.env.PORT || 3000;
+
+bookRouter.route('/books').get((req, res) => {
+  const response = { hello: 'This is my api'};
+  res.json(response);
+});
+
+app.use('/api', bookRouter);
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my Nodemon api');
+});
+
+app.listen(port, () => {
+  console.log('Running on port: ' + port);
+});
