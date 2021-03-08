96b16d541dd733a0c3638c495c33c509e996d681
jmartinezcl <martinez.alvarez.jose77@gmail.com>
2021-03-08 09:58:07 -0300

    add books data

---
+++ booksJson.js
@@ -0,0 +1,38 @@
+db.books.insert([
+    {
+        title:'"Eloquent JavaScript, Second Edition',
+        genre:'javascript',
+        author:'Marijn Haverbeke',
+        read: false
+    },
+    {
+        title:'Learning JavaScript Design Patterns',
+        genre:'design',
+        author:'Addy Osmani',
+        read: false
+    },
+    {
+        title:'Speaking JavaScript',
+        genre:'javascript',
+        author:'Axel Rauschmayer',
+        read: false
+    },
+    {
+        title:'Programming JavaScript Applications',
+        genre:'javascript',
+        author:'Eric Elliott',
+        read: true
+    },
+    {
+        title:'Understanding ECMAScript 6',
+        genre:'javascript',
+        author:'Nicholas C. Zakas',
+        read: true
+    },
+    {
+        title:'"Designing Evolvable Web APIs with ASP.NET',
+        genre:'.NET',
+        author:'Glenn Block, et al.',
+        read: false
+    },
+]);
