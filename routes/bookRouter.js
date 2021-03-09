f8fbc98991187dad3b205da02db113269d0bb653
jmartinezcl <martinez.alvarez.jose77@gmail.com>
2021-03-09 12:56:39 -0300

    add middleware

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -21,27 +21,30 @@ function routes(Book){
         });  
     });
 
-    bookRouter.route('/books/:bookId').get((req, res) => {  
+    bookRouter.use('/books/:bookId', (req, res, next) => {
         Book.findById(req.params.bookId, (err, book) => {
             if(err){
                 return res.send(err);
             }
-            return res.json(book);    
-        });  
-    })
-    .put((req, res) => {
-        Book.findById(req.params.bookId, (err, book) => {
-            if(err){
-                return res.send(err);
+            if(book){
+                req.book = book;
+                return next();
             }
-            book.title = req.body.title;
-            book.author = req.body.author;
-            book.genre = req.body.genre;
-            book.read = req.body.read;
-            book.save();
-            return res.json(book);    
+            return res.sendStatus(404);    
         });  
     });
+    bookRouter.route('/books/:bookId').
+    get((req, res) => res.json(req.book))
+    .put((req, res) => {
+        const { book } = req;
+        book.title = req.body.title;
+        book.author = req.body.author;
+        book.genre = req.body.genre;
+        book.read = req.body.read;
+        book.save();
+        return res.json(book);    
+         
+    });    
 
     return bookRouter;
 }
