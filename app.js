ca8d1eedae53c75ac3848b0489f03c7ef7819cf2
jmartinezcl <martinez.alvarez.jose77@gmail.com>
2021-03-10 17:45:55 -0300

    it was added test integration

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
-const db = mongoose.connect('mongodb://localhost/bookAPI');
+
+if(process.env.ENV === 'Test'){
+  console.log('\x1b[36m', 'This is a Test...' ,'\x1b[0m');
+  const db = mongoose.connect('mongodb://localhost/bookAPI_Test');
+}else {
+  console.log('\x1b[36m', 'This is for real...' ,'\x1b[0m');
+  const db = mongoose.connect('mongodb://localhost/bookAPI');
+}
+
 const port = process.env.PORT || 3000;
 const Book = require('./models/bookModel');
 const bookRouter = require('./routes/bookRouter')(Book);
@@ -13,9 +21,11 @@ app.use(express.json());
 app.use('/api', bookRouter);
 
 app.get('/', (req, res) => {
-  res.send('Welcome to my Nodemon api');
+  res.send('Welcome to API Books');
 });
 
-app.listen(port, () => {
-  console.log('Running on port: ' + port);
+app.server = app.listen(port, () => {
+  console.log(`Running on port: ${port}`);
 });
+
+module.exports = app;
