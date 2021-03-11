52347e9151a5e95587dc00f61b1be8199f549a0b
jmartinezcl <martinez.alvarez.jose77@gmail.com>
2021-03-11 11:14:06 -0300

    add HATEOAS

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -4,7 +4,7 @@ const booksController = require('../controllers/booksController');
 function routes(Book){
     const bookRouter = express.Router();
     const controller = booksController(Book);
-    
+
     bookRouter.route('/books')
     .post(controller.post)
     .get(controller.get);
@@ -22,7 +22,13 @@ function routes(Book){
         });  
     });
     bookRouter.route('/books/:bookId').
-    get((req, res) => res.json(req.book))
+    get((req, res) => {
+        const returnBook = req.book.toJSON();
+        returnBook.links = {};
+        const genre = req.book.genre.replace(' ', '%20');
+        returnBook.links.FilterByThisGenre = `http://${req.headers.host}/api/books/?genre=${genre}`;
+        res.json(returnBook);
+    })
     .put((req, res) => {
         const { book } = req;
         book.title = req.body.title;
